a212b60a9e0d290a3da118292b013259b7c94dc5
Tiago Ribeiro <tiago.ribeiro@360imprimir.pt>
2016-07-04 15:35:15 +0100

    # Elmah.Extensions
    * Change: Remove log to event log

---
+++ Elmah.Extensions/Elmah.Extensions/ErrorLogModule.cs
@@ -0,0 +1,48 @@
+﻿using Elmah.Extensions.Exceptions;
+using System;
+using System.Threading;
+using System.Web;
+
+namespace Elmah.Extensions
+{
+    public class ErrorLogModule : Elmah.ErrorLogModule, IHttpModule
+    {
+        static int _unhandledExceptionCount = 0;
+
+        static object _initLock = new object();
+        static bool _initialized = false;
+
+        public void Init(HttpApplication app)
+        {
+
+            // Do this one time for each AppDomain.
+            if (!_initialized)
+            {
+                lock (_initLock)
+                {
+                    if (!_initialized)
+                    {
+                        AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(OnUnhandledException);
+
+                        _initialized = true;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        void OnUnhandledException(object o, UnhandledExceptionEventArgs e)
+        {
+            // Let this occur one time for each AppDomain.
+            if (Interlocked.Exchange(ref _unhandledExceptionCount, 1) != 0)
+                return;
+
+            //Logar o erro para o elmah, criando uma UnhandledException com a Inner exception com a excepção que ocorreu, para que seja fácil identificar que esta excepção não foi tratada
+            this.LogException(new UnhandledException((Exception)e.ExceptionObject), HttpContext.Current);
+        }
+
+    }
+}
---
+++ Elmah.Extensions/Elmah.Extensions/Exceptions/UnhandledException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Elmah.Extensions.Exceptions
+{
+    [Serializable]
+    public class UnhandledException : Exception
+    {
+        public UnhandledException(Exception innerException): base("Unhandled exception occurred. Check the Inner Exception.", innerException)
+        {
+
+        }
+    }
+}
